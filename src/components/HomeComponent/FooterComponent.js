7c8bcc0b925512ece9f9b5587cf887da4120bf2c
DEEPIKA REDDY CHUDI <chudi.d@northeastern.edu>
2021-04-16 16:02:40 -0400

    Footer

--- src/components/HomeComponent/FooterComponent.js
+++ src/components/HomeComponent/FooterComponent.js
@@ -11,74 +11,50 @@ import {
 
 const FooterPage = () => {
   return (
-    <MDBFooter style={{backgroundColor: "black"}} className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-left text-md-left" style={{color: "white"}} >
+    <MDBFooter style={{backgroundColor: " #011422"}} className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-left text-md-left" style={{color: "grey"}} >
         <MDBRow>
-          <MDBCol md="4">
-            <h5 className="title ml-5" >About Us</h5>
+          <MDBCol md="3">
             <p align="justify" className='ml-5'> 
-            FlixFarm is the world's leading streaming entertainment service with over 167 million paid memberships in over 190 countries feature films across a wide variety of genres and languages. 
-            </p>
+            <FontAwesomeIcon icon="map-marked" color="grey" /> &nbsp;
+                <span>203 Fake St. Mountain View, <br></br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Boston, Massachusetts, <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  USA - 02215</span><br></br>
+              </p>
           </MDBCol>
           <MDBCol md="2" className='ml-5'>
-            <h5 className="title" >Genres</h5>
-            <ul class="list-unstyled">
-              <li className="list-unstyled">
-                <a href="#!">Action</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Comedy</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Science fiction</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Thriller</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Drama</a>
-              </li>
-            </ul>
+            <FontAwesomeIcon icon="phone-alt" color="grey" /> &nbsp;
+              <span>+1 789 456 1230</span><br></br>
           </MDBCol>
           <MDBCol md="3" className='ml-2'>
-            <h5 className="title">Contact us</h5>
+            
             <div>
-              <FontAwesomeIcon icon="map-marked" color="#FFFFFF" /> &nbsp;
-              <span>203 Fake St. Mountain View, <br></br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Boston, Massachusetts, <br></br>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  USA - 02215</span><br></br>
 
-              <FontAwesomeIcon icon="phone-alt" color="#FFFFFF" /> &nbsp;
-              <span>+1 789 456 1230</span><br></br>
-
-              <FontAwesomeIcon icon="envelope" color="#FFFFFF" /> &nbsp;
-              <a href="mailto:vmande94@gmail.com, abajithramesh13@gmail.com, abhiwase1995@gmail.com" target = "gmail.com">flixfarm@gmail.com</a>
-
-              
+              <FontAwesomeIcon icon="envelope" color="grey" /> &nbsp;
+              <a href="mailto:bhabhe.a@northeastern.edu, chudi.d@northeastern.edu, ananthula.sh@northeastern.edu" target = "gmail.com" >movies@gmail.com</a>
             </div>
           </MDBCol> 
           <MDBCol md="2" className='ml-1'>
-            <h5 className="title" align="left">Follow us on</h5>
             <a href="https://www.youtube.com/"
-              className="youtube social" style={{color: "#eb3223"}}>
+              className="youtube social" style={{color: "grey"}}>
               <FontAwesomeIcon icon={faYoutube} size="2x" />
             </a>&nbsp;&nbsp;
             <a href="https://www.facebook.com/"
-              className="facebook social" style={{color: "#black"}}>
+              className="facebook social" style={{color: "grey"}}>
               <FontAwesomeIcon icon={faFacebook} size="2x" />
             </a>&nbsp;&nbsp;
             <a href="https://www.twitter.com/" 
-              className="twitter social" style={{color: "#49a1eb"}}>
+              className="twitter social" style={{color: "grey"}}>
               <FontAwesomeIcon icon={faTwitter} size="2x" />
             </a>&nbsp;&nbsp;
             <a href="https://www.instagram.com/"
-              className="instagram social" style={{color: "#eb3223"}}>
+              className="instagram social" style={{color: "grey"}}>
               <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid style={{color: "white"}}>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.netflix.com/"> FlixFarm.com </a>
+      <div className="footer-copyright text-center py-2">
+        <MDBContainer fluid style={{color: "grey"}}>
+          &copy; {new Date().getFullYear()} Copyright: <a href="#"> Movies.com </a>
         </MDBContainer>
       </div>
     </MDBFooter>
